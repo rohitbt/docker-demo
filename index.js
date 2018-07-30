da1984abe05f89b54ae400940565a9f24d74d47b
rohitbt <40511303+rohitbt@users.noreply.github.com>
2018-07-30 11:54:42 +0530

    commit23

--- index.js
+++ index.js
@@ -9,5 +9,3 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-})
